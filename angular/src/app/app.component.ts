f55821c4213bc9336bb39c57018f3eb6fc03da67
Christopher Guzowski <coderchris141@gmail.com>
2024-10-10 14:00:17 -0400

    Cleaned and reorganized CSS files

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/staticComponents/navbar/navbar.component';
+import { AboutComponent } from './core/staticComponents/about/about.component';
+import { ConnectComponent } from './core/staticComponents/connect/connect.component';
+import { BlogComponent } from './core/staticComponents/blog/blog.component';
+import { ProjectComponent } from './core/staticComponents/project/project.component';
 import { ThemeService } from './core/singletonServices/theme.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, ConnectComponent, BlogComponent, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/src/app/core/staticComponents/about/about.component.ts
+++ angular/src/app/core/staticComponents/about/about.component.ts
@@ -1,12 +1,17 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
 
+  name: string = 'John Doe';
+  bio: string = 'I am a software developer with a passion for creating dynamic and responsive web applications.';
+  skills: string[] = ['Angular', 'TypeScript', 'JavaScript', 'HTML', 'CSS'];
+  hobbies: string[] = ['Reading', 'Traveling', 'Photography', 'Cooking'];
 }

1c98b343004d7d06e58e54dbcdc9bae0e13fbab7
Christopher Guzowski <coderchris141@gmail.com>
2025-02-21 11:28:21 -0500

    finished projects section, now onto working on backend

--- angular/src/app/core/staticComponents/project/project.component.ts
+++ angular/src/app/core/staticComponents/project/project.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { INPUT_MODALITY_DETECTOR_DEFAULT_OPTIONS } from '@angular/cdk/a11y';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -7,6 +9,11 @@ import { Component } from '@angular/core';
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
+  message: string = 'Welcome or welcome back user # (backend coming soon)';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+  }
 }
